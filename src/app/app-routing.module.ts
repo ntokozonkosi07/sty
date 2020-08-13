3f208472913dc0059450e6f870f949c9658d4a09
Siya Nkosi <ntokozonkosi07@gmail.com>
2020-08-13 22:18:50 +0200

    set active route on menus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductsComponent } from './pages/products/products.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', component: ProductsComponent}
+  {path: 'products', pathMatch: 'full', component: ProductsComponent}
 ];
 
 @NgModule({
